75638b6ab1dd7af0d56065529961b43d79668835
alex <alex@pdp7.net>
2016-03-15 22:27:45 +0100

    Fix broken build due to not running tests before committing :(

--- v2f-core/src/test/java/net/pdp7/v2f/core/RouterTest.java
+++ v2f-core/src/test/java/net/pdp7/v2f/core/RouterTest.java
@@ -6,11 +6,11 @@ import net.pdp7.v2f.core.Router.ListTableRoute;
 public class RouterTest extends TestCase {
 
 	public void testRouter() {
-		ListTableRoute route = (ListTableRoute) new Router(null, null).findRoute("/table_name/");
+		ListTableRoute route = (ListTableRoute) new Router(null, null, null).findRoute("/table_name/");
 		assertEquals("table_name", route.table);
 	}
 
 	public void testDetailRoute() {
-		assertFalse(new Router(null, null).getDetailRoute("table", "funky/id").contains("funky/id"));
+		assertFalse(new Router(null, null, null).getDetailRoute("table", "funky/id").contains("funky/id"));
 	}
 }
