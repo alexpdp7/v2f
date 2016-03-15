c98b296dbe5379c2631127e3ffb2274b6b4e5c11
alex <alex@pdp7.net>
2016-03-15 22:27:15 +0100

    Fix formatting bug that slipped by

--- v2f-core/src/main/java/net/pdp7/v2f/core/DetailHandler.java
+++ v2f-core/src/main/java/net/pdp7/v2f/core/DetailHandler.java
@@ -44,7 +44,7 @@ public class DetailHandler {
 					table,
 					id == null ? null : loadRecord(table, id),
 					id == null ? "0" : null);
-			ImmutableMap<String, ?> model = new ImmutableMap.Builder<String,Object>()
+			ImmutableMap<String, ?> model = new ImmutableMap.Builder<String, Object>()
 					.put("row", row)
 					.put("success_url", router.getListTableRoute(table))
 					.build();
